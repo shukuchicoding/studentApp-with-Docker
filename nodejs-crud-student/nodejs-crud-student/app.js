82418f32215afe7be74b29ba9ef24ee57c9d528f
Lê Minh Việt Anh <hinhmomohinh@gmail.com>
2024-11-27 16:20:02 +0700

    https

--- nodejs-crud-student/nodejs-crud-student/app.js
+++ nodejs-crud-student/nodejs-crud-student/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://lmvait2k66.id.vn',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
     credentials: true  // If you want to allow cookies or authentication headers
 }));
